884f99738e3de4ccc9ba3f06971e1840302d7ef4
bfeelin <brianphelan59@gmail.com>
2023-12-26 19:02:39 -0500

    Add stoplight states, setTimeout on useEffect to trigger an infinite loop to continuously trigger the next stoplight state

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+interface StoplightState {
+  value: number,
+  delay: number
+}
+
+const stoplightStates:StoplightState[] = [
+  {
+    value: 0,
+    delay: 5000
+  },
+  {
+    value: 1,
+    delay: 2000
+  },
+  {
+    value: 2,
+    delay: 5000
+  }
+]
+
 function App() {
-  const [stoplightState, setStoplightState] = useState("go")
+  const [stoplightState, setStoplightState] = useState<number>(0)
 
   useEffect(() => {
+    const nextState:StoplightState = stoplightStates[(stoplightState+1) % stoplightStates.length]
+    const timeoutId = setTimeout(() => {
+      setStoplightState(nextState.value);
+    }, nextState.delay);
 
-  }, [])
+    return () => clearTimeout(timeoutId);
+  }, [stoplightState])
 
   return (
     <>
       <div className='container'>
-        <div className='light' style={{backgroundColor: stoplightState === "stop" ? 'red' : ''}}>
+        <div className='light' style={{backgroundColor: stoplightState === 0 ? 'red' : ''}}>
 
         </div>
-        <div className='light'  style={{backgroundColor: stoplightState === "slow" ? 'orange' : ''}}>
+        <div className='light'  style={{backgroundColor: stoplightState === 1 ? 'orange' : ''}}>
           
         </div>
-        <div className='light'  style={{backgroundColor: stoplightState === "go" ? 'green' : ''}}>
+        <div className='light'  style={{backgroundColor: stoplightState === 2 ? 'green' : ''}}>
           
         </div>
       </div>
